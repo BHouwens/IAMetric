fce84b3b90369f8eb937a1dd30e1a0b4b9fd7ea3
BHouwens <zhion360@gmail.com>
2016-07-15 11:52:09 +0200

    Setting up some of the dot-bubble chart

---
+++ src/utils/dotChart.js
@@ -0,0 +1,57 @@
+import d3 from 'd3';
+import { scaleOrdinal } from 'd3-scale';
+import { axisLeft, axisBottom } from 'd3-axis';
+
+export class DotChart {
+
+    /**
+     *  A dot-bubble chart to display discrete data
+     * 
+     *  @param {string} element - Element to attach the chart to
+     */
+
+    constructor(element) {
+        this.x;
+        this.y;
+        this.element = element;
+        this.width = 400;
+        this.height = 300;
+
+        this.init();
+    }
+
+
+    /**
+     *  Initialises the chart
+     */
+
+    init() {
+        let { element, width, height } = this;
+
+        this.chart = d3.select(element)
+                       .attr('width', width)
+                       .attr('height', height);
+
+        this.x = d3.scaleOrdinal()
+                   .domain(['A', 'B'])
+                   .range([0, 1]);
+
+        this.y = d3.scaleOrdinal()
+                   .domain(['X', 'Y'])
+                   .range([0, 1]);
+
+        this.constructAxes();
+    }
+
+
+    /**
+     *  Constructs and bolts on axes to chart
+     */
+
+    constructAxes() {
+        let { x, y } = this;
+
+        this.xAxis = 
+    }
+
+}
