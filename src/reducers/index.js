2c74f65d022fdee153f3c83efe036b7741fdc756
BHouwens <zhion360@gmail.com>
2016-08-11 20:58:52 +0200

    Fixing up minor issues and removing PainPoint

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { bubbleMatrix } from './bubbleMatrixReducer';
 import { averageMetric } from './averageMetricReducer';
-import { painPointButton } from './painPointButtonReducer';
 
 export const rootReducer = combineReducers({
     bubbleMatrix,
-    averageMetric,
-    painPointButton
+    averageMetric
 });
